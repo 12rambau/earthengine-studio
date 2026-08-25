85cee3458455c0ea8278231e9b1101266b537292
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2026-08-25 16:56:25 +0000

    feat: add a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
+  <v-app class="studio-shell">
+    <v-app-bar
+      class="studio-header"
+      :elevation="0"
+      height="56"
+    >
+      <div class="studio-header__brand">
+        <v-icon
+          color="primary"
+          icon="mdi-earth"
+          size="22"
+        />
+        <span>Earth Engine Studio</span>
+      </div>
+
+      <v-spacer />
+
+      <v-tooltip text="Toggle theme">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            aria-label="Toggle theme"
+            icon="mdi-theme-light-dark"
+            variant="text"
+            @click="$vuetify.theme.cycle()"
+          />
+        </template>
+      </v-tooltip>
+    </v-app-bar>
+
+    <v-main class="studio-main">
+      <main
+        class="studio-canvas"
+        aria-label="Workspace canvas"
+      />
     </v-main>
-    <v-btn
-      class="m-2"
-      icon="mdi-theme-light-dark"
-      location="top right"
-      position="absolute"
-      @click="$vuetify.theme.cycle()"
-    />
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
